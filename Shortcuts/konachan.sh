759cd8f862b3d4477ef1b55eb4ec73f20d41eb98
Hangyi Wu <hwu107@syr.edu>
2019-06-28 00:25:47 -0400

    delete one shell script file

--- Shortcuts/konachan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo $1 | pbcopy
-
-[ "$#" -lt 1 ] && echo "Usage: String you want to be cut." && exit 0
-
-osascript -e 'delay 0.5' -e 'tell application "Keyboard Maestro Engine"' -e 'do script "B40672AB-7AF3-4D4F-AD17-8F54A6087FB2"' -e 'end tell' &
